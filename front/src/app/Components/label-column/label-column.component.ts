541930bd189386bede36fef3583b91ca070dd7c2
AlexPracticas\Alex <alejandrosainzsainz@outlook.es>
2026-05-18 14:58:48 +0200

    Creación del label-column-componente. El código se desarrollará en su propia rama.

---
+++ front/src/app/Components/label-column/label-column.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-label-column',
+  imports: [],
+  templateUrl: './label-column.component.html',
+  styleUrl: './label-column.component.css',
+})
+export class LabelColumnComponent {}
